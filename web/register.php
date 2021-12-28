866445e5635f14dd0fc0eef58f695a9086cb77b4
laharanick <laharanick@gmail.com>
2021-12-28 17:58:36 +0700

    it was born

---
+++ web/register.php
@@ -0,0 +1,482 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>register</title>
+</head>
+
+<body background="young-wild-free.png">
+<table background="h copy.png" width="906" height="161" border="0" align="center">
+  <tr>
+    <td colspan="1" bgcolor=""><p>&nbsp;</p>
+      <table bgcolor="" width="250" border="0" cellspacing="0" cellpadding="0" align="right" >
+          <tr>
+            <td width="88" height="24" align="left" valign="middle"><strong><font color="white">User Name </strong></td>
+            <td width="12" align="left" valign="middle"><strong><font color="white">:</strong></td>
+            <td width="150" align="left" valign="middle"><label>
+              <input name="username" type="text" id="username" />
+            </label></td>
+          </tr>
+          <tr>
+            <td height="25" align="left" valign="middle"><strong><font color="white">Password</strong></td>
+            <td align="left" valign="middle"><strong><font color="white">:</strong></td>
+            <td align="left" valign="middle"><label>
+              <input name="password" type="password" id="password" />
+            </label></td>
+          </tr>
+          <tr>
+            <td height="27" align="left" valign="top">&nbsp;</td>
+            <td align="left" valign="top">&nbsp;</td>
+            <td align="left" valign="top"><label><a href="index.html">
+              <input type="submit" name="Submit" value="LogIn" /></a>
+              &nbsp;&nbsp;<a href="register.php"><font color="white">Registrasi</a></label></td>
+          </tr>
+      </table></td>
+  </tr>
+  <tr valign="middle" bgcolor="">
+    <td height="24" colspan="7" align="right" bgcolor=""><table align="right">
+      <tr>
+        <td><input name="search" type="text" dir="ltr" lang="id" value="" size="45" />
+            <a href="search.html"><img src="file:///C|/Users/Nick lahara/Videos/cloud-and-loupe-binary-code-search-Download-Royalty-free-Vector-File-EPS-18250.jpg" width="30" height="23" align="top" /></a></td>
+      </tr>
+    </table>
+        <embed name="CS1427621495344" src="Bohemian Rhapsody - Steve Bean - Classical Guitar.mp3" loop="false" autostart="false" hidden="true" width="0" height="0" enablejavascript="true"></embed>
+    </td>
+  </tr>
+</table>
+<form action="Untitled-4.php" method="post" name="registrasi" target="_self">
+  <table width="446" height="52" border="0" align="center"><
+  <tr align="right">
+    <td width="112" height="24" align="left"><font color="white">Nama<font color="#FFFFFF"></div></td>
+    <td width="50" height="24"align="center"><font color="white">:</td>
+    <td width="270" align="left"><input name="Nama" type="text" dir="ltr" lang="id" size="45" /></td>
+  </tr>
+  <tr> 
+  <td width="112" height="10"align="left"><font color="white">Tempat lahir</td>
+  <td width="50" height="10"align="center"><font color="white">:</td>
+  <td width="270" height="10"align="left"><input name="Nama" type="text" dir="ltr" lang="id" size="45" /></td>
+  </tr>
+  <tr>
+  <td width="112" height="10"align="left"><font color="white">Tanggal Lahir</td>
+  <td width="50" height="10"align="center"><font color="white">:</td>
+  <td><label>
+    <select name="select" id="select">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+      <option value="9">9</option>
+      <option value="10">10</option>
+      <option value="11">11</option>
+      <option value="12">12</option>
+      <option value="13">13</option>
+      <option value="14">14</option>
+      <option value="15">15</option>
+      <option value="16">16</option>
+      <option value="17">17</option>
+      <option value="18">18</option>
+      <option value="19">19</option>
+      <option value="20">20</option>
+      <option value="21">21</option>
+      <option value="22">22</option>
+      <option value="23">23</option>
+      <option value="24">24</option>
+      <option value="25">25</option>
+      <option value="26">26</option>
+      <option value="27">27</option>
+      <option value="28">28</option>
+      <option value="29">29</option>
+      <option value="30">30</option>
+      <option value="31">31</option>
+        </select>
+    &nbsp;&nbsp;&nbsp; 
+     &nbsp;<select name="select2" id="select2">
+          <option value="Januari">Januari</option>
+          <option value="Februari">Februari</option>
+          <option value="Maret">Maret</option>
+          <option value="April">April</option>
+          <option value="Mei">Mei</option>
+          <option value="Juni">Juni</option>
+          <option value="Juli">Juli</option>
+          <option value="Agustus">Agustus</option>
+          <option value="September">September</option>
+          <option value="Oktober">Oktober</option>
+          <option value="November">November</option>
+          <option value="Desember">Desember</option>
+        </select>
+    &nbsp;&nbsp;  &nbsp;
+    <select name="select3" id="select3">
+          <option value="1990">1990</option>
+          <option value="1991">1991</option>
+          <option value="1992">1992</option>
+          <option value="1993">1993</option>
+          <option value="1994">1994</option>
+          <option value="1995">1995</option>
+          <option value="1996">1996</option>
+          <option value="1997">1997</option>
+          <option value="1998">1998</option>
+          <option value="1999">1999</option>
+          <option value="2000">2000</option>
+          <option value="2001">2001</option>
+          <option value="2002">2002</option>
+          <option value="2003">2003</option>
+          <option value="2004">2004</option>
+          <option value="2005">2005</option>
+          <option value="2006">2006</option>
+          <option value="2007">2007</option>
+          <option value="2008">2008</option>
+          <option value="2009">2009</option>
+          <option value="2010">2010</option>
+        </select>
+  </label></td></tr>
+  <tr><td width="112" height="24" align="left"><font color="white">Agama</div></td>
+    <td width="50" height="24"align="center"><font color="white">:</td>
+    <td width="270" align="left"><label>
+      <select name="select4" id="select4">
+        <option value="Islam">Islam</option>
+        <option value="Khatolik">Khatolik</option>
+        <option value="Protestan">Protestan</option>
+        <option value="Hindu">Hindu</option>
+        <option value="Buddha">Buddha</option>
+        <option value="Konghu chu">Konghu chu</option>
+        </select>
+    </label></td>
+</tr>
+<tr><td width="112" height="24" align="left"><font color="white">Jenis Kelamin</div></td>
+    <td width="50" height="24"align="center"><font color="white">:</td>
+     <td height="24" colspan="4"align="left"><input name="pria" type="radio" value="Laki - laki"><font color="white"> 
+      Laki - laki &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <input name="women" type="radio" value="wanita" /><font color="white">Perempuan</td>
+    </tr>  
+    <tr>
+    <td width="112" height="24" align="left"><font color="white">Alamat</div></td>
+    <td width="50" height="24"align="center"><font color="white">:</td>
+    <td width="50" height="24"align="center"><textarea name="alamat" cols="42" rows="" dir="ltr">Jl.</textarea></td></tr>
+    <tr>
+    <td width="112" height="24" align="left"><font color="white">No Telp</div></td>
+    <td width="50" height="24"align="center"><font color="white">:</td>
+    <td width="50" height="24"align="leff"><input name="no" type="text" dir="ltr" lang="id" value="+62" size="45" /></td>
+</tr>
+<tr><td width="112" height="24" align="left"><font color="white">Kode Pos</div></td>
+    <td width="50" height="24"align="center"><font color="white">:</td>
+    <td width="50" height="24"align="left"><input name="no" type="text" dir="ltr" lang="id" value="" size="45" /></td>
+</tr>
+<tr><td width="112" height="24" align="left"><font color="white">Profesi</div></td>
+    <td width="50" height="24"align="center"><font color="white">:</td>
+    <td width="50" height="24"align="left"><input name="no" type="text" dir="ltr" lang="id" value="" size="45" /></td>
+</tr>
+<tr><td width="112" height="24" align="left"><font color="white">Golongan Darah</div></td>
+    <td width="50" height="24"align="center"><font color="white">:</td>
+    <td width="50" height="24"align="left"><label>
+      <select name="select5" id="select5">
+        <option value="O">O</option>
+        <option value="A">A</option>
+        <option value="B">B</option>
+        <option value="AB">AB</option>
+      </select>
+    </label></td>
+</tr>
+</table>
+  <p align="center"><a href="index.html"><img src="g.png" width="158" height="57" /> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="register.php"><img src="r.png" width="158" height="57" /></a>
+<table width="906" border="0" align="center">
+    <tr>
+      <td height="30" colspan="2" align="center" valign="middle" bgcolor="#993300"><span class="style9"><strong><font face="comic sans ms" color="#FFFFFF"><marquee behavior="alternate">Copyright &copy; X RPL 2 - 2015 By : Nico Gawa Lahara</font> </strong></span></td>
+    </tr>
+  </table>
+  <p align="center">
+  </form>
+   <script type='text/javascript'>
+
+if ((document.getElementById) &&
+window.addEventListener || window.attachEvent){
+
+(function(){
+
+//Configure here.
+
+var num = 50; //Number of flakes
+var timer = 50; //setTimeout speed. Varies on different comps
+var enableinNS6 = 0; //Enable script in NS6/Mozilla? Snow animation could be slow in those browsers. (1=yes, 0=no).
+
+//End.
+
+var y = [];
+var x = [];
+var fall = [];
+var theFlakes = [];
+var sfs = [];
+var step = [];
+var currStep = [];
+var h,w,r;
+var d = document;
+var pix = "px";
+var domWw = (typeof window.innerWidth == "number");
+var domSy = (typeof window.pageYOffset == "number");
+var idx = d.getElementsByTagName('div').length;
+
+if (d.documentElement.style &&
+typeof d.documentElement.style.MozOpacity == "string")
+num = 12;
+
+for (i = 0; i < num; i++){
+sfs[i] = Math.round(1 + Math.random() * 1);
+
+document.write('<div id="flake'+(idx+i)+'" style="position:absolute;top:0px;left:0px;width:'
++sfs[i]+'px;height:'+sfs[i]+'px;background-color:#ffffff;font-size:'+sfs[i]+'px"><\/div>');
+
+currStep[i] = 0;
+fall[i] = (sfs[i] == 1)?
+Math.round(2 + Math.random() * 2): Math.round(3 + Math.random() * 2);
+step[i] = (sfs[i] == 1)?
+0.05 + Math.random() * 0.1 : 0.05 + Math.random() * 0.05 ;
+}
+
+
+if (domWw) r = window;
+else{
+if (d.documentElement &&
+typeof d.documentElement.clientWidth == "number" &&
+d.documentElement.clientWidth != 0)
+r = d.documentElement;
+else{
+if (d.body &&
+typeof d.body.clientWidth == "number")
+r = d.body;
+}
+}
+
+
+function winsize(){
+var oh,sy,ow,sx,rh,rw;
+if (domWw){
+if (d.documentElement && d.defaultView &&
+typeof d.defaultView.scrollMaxY == "number"){
+oh = d.documentElement.offsetHeight;
+sy = d.defaultView.scrollMaxY;
+ow = d.documentElement.offsetWidth;
+sx = d.defaultView.scrollMaxX;
+rh = oh-sy;
+rw = ow-sx;
+}
+else{
+rh = r.innerHeight;
+rw = r.innerWidth;
+}
+h = rh - 2;
+w = rw - 2;
+}
+else{
+h = r.clientHeight - 2;
+w = r.clientWidth - 2;
+}
+}
+
+
+function scrl(yx){
+var y,x;
+if (domSy){
+y = r.pageYOffset;
+x = r.pageXOffset;
+}
+else{
+y = r.scrollTop;
+x = r.scrollLeft;
+}
+return (yx == 0)?y:x;
+}
+
+
+function snow(){
+var dy,dx;
+
+for (i = 0; i < num; i++){
+dy = fall[i];
+dx = fall[i] * Math.cos(currStep[i]);
+
+y[i]+=dy;
+x[i]+=dx;
+
+if (x[i] >= w || y[i] >= h){
+y[i] = -10;
+x[i] = Math.round(Math.random() * w);
+fall[i] = (sfs[i] == 1)?
+Math.round(2 + Math.random() * 2): Math.round(3 + Math.random() * 2);
+step[i] = (sfs[i] == 1)?
+0.05 + Math.random() * 0.1 : 0.05 + Math.random() * 0.05 ;
+}
+
+theFlakes[i].top = y[i] + scrl(0) + pix;
+theFlakes[i].left = x[i] + scrl(1) + pix;
+
+currStep[i]+=step[i];
+}
+setTimeout(snow,timer);
+}
+
+
+function init(){
+winsize();
+for (i = 0; i < num; i++){
+theFlakes[i] = document.getElementById("flake"+(idx+i)).style;
+y[i] = Math.round(Math.random()*h);
+x[i] = Math.round(Math.random()*w);
+}
+snow();
+}
+
+
+if (window.addEventListener){
+window.addEventListener("resize",winsize,false);
+window.addEventListener("load",init,false);
+}
+else if (window.attachEvent){
+window.attachEvent("onresize",winsize);
+window.attachEvent("onload",init);
+}
+
+})();
+}//tamat
+</script>
+  <style type="text/css">
+/* Circle Text Styles */
+#outerCircleText {
+/* Optional - DO NOT SET FONT-SIZE HERE, SET IT IN THE SCRIPT */
+font-style:italic;
+font-weight: bold;
+font-family: elephant;
+color: gold;
+
+
+
+/* End Optional */
+
+/* Start Required - Do Not Edit */
+position: absolute;top: 0;left: 0;z-index: 3000;cursor:default;}
+#outerCircleText div {position: relative;}
+#outerCircleText div div {position: absolute;top: 0;left: 0;text-align: center;}
+/* End Required */
+/* End Circle Text Styles */
+</style>
+<script type="text/javascript">
+
+/* Circling text trail- Tim Tilton
+Website: http://www.tempermedia.com/
+Visit: http://www.dynamicdrive.com/ for Original Source and tons of scripts
+Modified Here for more flexibility and modern browser support
+Modifications as first seen in http://www.dynamicdrive.com/forums/
+username:jscheuer1 - This notice must remain for legal use
+*/
+
+;(function(){
+
+// Your message here (QUOTED STRING)
+var msg = "Nick";
+
+/* THE REST OF THE EDITABLE VALUES BELOW ARE ALL UNQUOTED NUMBERS */
+
+// Set font's style size for calculating dimensions
+// Set to number of desired pixels font size (decimal and negative numbers not allowed)
+var size = 24;
+
+// Set both to 1 for plain circle, set one of them to 2 for oval
+// Other numbers & decimals can have interesting effects, keep these low (0 to 3)
+var circleY = 0.75; var circleX = 1.75;
+
+// The larger this divisor, the smaller the spaces between letters
+// (decimals allowed, not negative numbers)
+var letter_spacing = 3;
+
+// The larger this multiplier, the bigger the circle/oval
+// (decimals allowed, not negative numbers, some rounding is applied)
+var diameter = 10;
+
+// Rotation speed, set it negative if you want it to spin clockwise (decimals allowed)
+var rotation = 0.2;
+
+// This is not the rotation speed, its the reaction speed, keep low!
+// Set this to 1 or a decimal less than one (decimals allowed, not negative numbers)
+var speed = 0.4;
+
+////////////////////// Stop Editing //////////////////////
+
+if (!window.addEventListener && !window.attachEvent || !document.createElement) return;
+
+msg = msg.split('');
+var n = msg.length - 1, a = Math.round(size * diameter * 0.208333), currStep = 20,
+ymouse = a * circleY + 20, xmouse = a * circleX + 20, y = [], x = [], Y = [], X = [],
+o = document.createElement('div'), oi = document.createElement('div'),
+b = document.compatMode && document.compatMode != "BackCompat"? document.documentElement : document.body,
+
+mouse = function(e){
+e = e || window.event;
+ymouse = !isNaN(e.pageY)? e.pageY : e.clientY; // y-position
+xmouse = !isNaN(e.pageX)? e.pageX : e.clientX; // x-position
+},
+
+makecircle = function(){ // rotation/positioning
+if(init.nopy){
+o.style.top = (b || document.body).scrollTop + 'px';
+o.style.left = (b || document.body).scrollLeft + 'px';
+};
+currStep -= rotation;
+for (var d, i = n; i > -1; --i){ // makes the circle
+d = document.getElementById('iemsg' + i).style;
+d.top = Math.round(y[i] + a * Math.sin((currStep + i) / letter_spacing) * circleY - 15) + 'px';
+d.left = Math.round(x[i] + a * Math.cos((currStep + i) / letter_spacing) * circleX) + 'px';
+};
+},
+
+drag = function(){ // makes the resistance
+y[0] = Y[0] += (ymouse - Y[0]) * speed;
+x[0] = X[0] += (xmouse - 20 - X[0]) * speed;
+for (var i = n; i > 0; --i){
+y[i] = Y[i] += (y[i-1] - Y[i]) * speed;
+x[i] = X[i] += (x[i-1] - X[i]) * speed;
+};
+makecircle();
+},
+
+init = function(){ // appends message divs, & sets initial values for positioning arrays
+if(!isNaN(window.pageYOffset)){
+ymouse += window.pageYOffset;
+xmouse += window.pageXOffset;
+} else init.nopy = true;
+for (var d, i = n; i > -1; --i){
+d = document.createElement('div'); d.id = 'iemsg' + i;
+d.style.height = d.style.width = a + 'px';
+d.appendChild(document.createTextNode(msg[i]));
+oi.appendChild(d); y[i] = x[i] = Y[i] = X[i] = 0;
+};
+o.appendChild(oi); document.body.appendChild(o);
+setInterval(drag, 25);
+},
+
+ascroll = function(){
+ymouse += window.pageYOffset;
+xmouse += window.pageXOffset;
+window.removeEventListener('scroll', ascroll, false);
+};
+
+o.id = 'outerCircleText'; o.style.fontSize = size + 'px';
+
+if (window.addEventListener){
+window.addEventListener('load', init, false);
+document.addEventListener('mouseover', mouse, false);
+document.addEventListener('mousemove', mouse, false);
+if (/Apple/.test(navigator.vendor))
+window.addEventListener('scroll', ascroll, false);
+}
+else if (window.attachEvent){
+window.attachEvent('onload', init);
+document.attachEvent('onmousemove', mouse);
+};
+
+})();
+
+</script>
+</body>
+</html>
